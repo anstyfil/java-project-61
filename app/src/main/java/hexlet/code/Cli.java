ef9f2e3279e3e3ea427967799884c5d12f5edb08
anstyfil <anstyfil@gmail.com>
2025-08-18 13:07:37 +0300

    add class Cli and integrate it in App

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,4 +1,15 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Cli {
+    public static void getByName() {
+        Scanner sc = new Scanner(System.in);
+
+         System.out.print("May i have your name? ");
+         String userName = sc.nextLine();
+         System.out.println("Hello, " + userName);
+
+         sc.close();
+    }
 }
