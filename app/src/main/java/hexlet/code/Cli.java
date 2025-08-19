b866611e432d78dc5efa867890f8cb260d4e4e32
anstyfil <anstyfil@gmail.com>
2025-08-19 17:35:59 +0300

    fix indentation per Checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class Cli {
     public static void getByName() {
         Scanner sc = new Scanner(System.in);
-
-         System.out.print("May i have your name? ");
-         String userName = sc.nextLine();
-         System.out.println("Hello, " + userName);
-
-         sc.close();
+        System.out.print("May i have your name? ");
+        String userName = sc.nextLine();
+        System.out.println("Hello, " + userName);
+        sc.close();
     }
 }
